b9cf0555233439dcea22e48609acb1abd9b25ab7
Stanley H Griggs II <shg5004@gmail.com>
2015-12-03 23:13:53 -0500

    Switched from BeautifulSoup to lxml
    The Microsoft Excel generated webpage markup was very awful and parsing
    using BeautifulSoup was not an optimal solution. Developer Paris
    Butterfield (http://parisbutterfield.com/#about) who randomly seen my
    social media frustration post suggested using XPATH to scrape. After
    reading documentation this solution works!

--- jailjawn.py
+++ jailjawn.py
@@ -1,12 +1,19 @@
+from lxml import html
 import requests
-from bs4 import BeautifulSoup
 
-url = "http://www.phila.gov/prisons/page.htm"
-r = requests.get(url)
+page = requests.get('http://www.phila.gov/prisons/page.htm')
+tree = html.fromstring(page.content)
 
-soup = BeautifulSoup(r.content)
+#Sample code to test implmentation
+facility = tree.xpath('//tr[14]/td[1]/text()')
+maleCount = tree.xpath('//tr[14]/td[2]/text()')
+openWard = tree.xpath('//tr[14]/td[12]/text()')
+total = tree.xpath('//tr[14]/td[16]/text()')
+hospitals = tree.xpath('//tr[14]/td[17]/text()')
 
-prison_data = soup.find_all("tr")
-
-for item in prison_data:
-    print item.get_text()
+#Results
+print 'Facility: ', facility
+print 'maleCount: ', maleCount
+print 'Open Ward: ', openWard
+print 'Total: ', total
+print 'Hospitals: ', hospitals
