a3aaede0f580b750bfdae2f331f0ec1b08c94f4a
Stanley H Griggs II <shg5004@gmail.com>
2015-12-07 18:24:07 -0500

    Removed unused code and added adultMaleCount

--- jailjawn.py
+++ jailjawn.py
@@ -2,28 +2,19 @@ from lxml import html
 import requests
 
 class Facility:
-    def __init__(self, facilityName):
+    def __init__(self, facilityName, adultMaleCount):
         self.facilityName =  facilityName
-        self.
+        self.adultMaleCount =  adultMaleCount
+
+
 page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
 
-f = Facility(tree.xpath('//tr[14]/td[1]/text()'))
-print(f.facilityName)
+f = Facility(tree.xpath('//tr[14]/td[1]/text()'),
+                tree.xpath('//tr[14]/td[2]/text()')
+                )
 
 
-#Sample code to test implmentation
-#facility = tree.xpath('//tr[14]/td[1]/text()')
-#maleCount = tree.xpath('//tr[14]/td[2]/text()')
-#openWard = tree.xpath('//tr[14]/td[12]/text()')
-#total = tree.xpath('//tr[14]/td[16]/text()')
-#hospitals = tree.xpath('//tr[14]/td[17]/text()')
-
-#Results
-#print 'Facility: ', facility
-#print 'maleCount: ', maleCount
-#print 'Open Ward: ', openWard
-#print 'Total: ', total
-#print 'Hospitals: ', hospitals
+print(f.facilityName, f.adultMaleCount)
 
 
