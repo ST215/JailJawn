4dac83246f0dd2564e260393d96cade63b9f1260
Stanley H Griggs II <shg5004@gmail.com>
2015-12-08 16:43:41 -0500

    Git_Issue: #6 Cleaned Formatting

--- jailjawn.py
+++ jailjawn.py
@@ -1,24 +1,25 @@
 from lxml import html
 import requests
 
+
 class Facility:
     def __init__(self, facilityName, adultMaleCount, adultFemaleCount, juvenileMaleCount, juvenileFemaleCount,
                  inCountOutCountMale, inCountOutCountFemale, workersMale, workersFemale, furloughMale,
                  furloughFemale, openWardMale, openWardFemale, emergTripsMale, emergTripsFemale):
 
-        self.facilityName =  facilityName
-        self.adultMaleCount =  adultMaleCount
-        self.adultFemaleCount =  adultFemaleCount
-        self.juvenileMaleCount =  juvenileMaleCount
-        self.juvenileFemaleCount =  juvenileFemaleCount
-        self.inCountOutCountMale =  inCountOutCountMale
-        self.inCountOutCountFemale =  inCountOutCountFemale
-        self.workersMale =  workersMale
-        self.workersFemale =  workersFemale
-        self.FurloughMale =  furloughMale
-        self.FurloughFemale =  furloughFemale
-        self.openWardMale =  openWardMale
-        self.openWardFemale =  openWardFemale
+        self.facilityName = facilityName
+        self.adultMaleCount = adultMaleCount
+        self.adultFemaleCount = adultFemaleCount
+        self.juvenileMaleCount = juvenileMaleCount
+        self.juvenileFemaleCount = juvenileFemaleCount
+        self.inCountOutCountMale = inCountOutCountMale
+        self.inCountOutCountFemale = inCountOutCountFemale
+        self.workersMale = workersMale
+        self.workersFemale = workersFemale
+        self.furloughMale = furloughMale
+        self.furloughFemale = furloughFemale
+        self.openWardMale = openWardMale
+        self.openWardFemale = openWardFemale
         self.emergTripsMale = emergTripsMale
         self.emergTripsFemale = emergTripsFemale
 
@@ -45,7 +46,7 @@ f = Facility(tree.xpath('//tr[14]/td[1]/text()'),
 
 
 print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount,
-      f.inCountOutCountMale, f.inCountOutCountFemale, f.workersMale, f.workersFemale, f.FurloughMale, f.FurloughFemale,
+      f.inCountOutCountMale, f.inCountOutCountFemale, f.workersMale, f.workersFemale, f.furloughMale, f.furloughFemale,
       f.openWardMale, f.openWardFemale, f.emergTripsMale, f.emergTripsFemale)
 
 
