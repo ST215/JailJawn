c66161036a03cd4b67139ca718e0fc3bfe4894f1
Stanley H Griggs II <shg5004@gmail.com>
2015-12-08 10:36:24 -0500

    Added juvenileFemaleCount

--- jailjawn.py
+++ jailjawn.py
@@ -2,11 +2,12 @@ from lxml import html
 import requests
 
 class Facility:
-    def __init__(self, facilityName, adultMaleCount, adultFemaleCount, juvenileMaleCount):
+    def __init__(self, facilityName, adultMaleCount, adultFemaleCount, juvenileMaleCount, juvenileFemaleCount):
         self.facilityName =  facilityName
         self.adultMaleCount =  adultMaleCount
         self.adultFemaleCount =  adultFemaleCount
         self.juvenileMaleCount =  juvenileMaleCount
+        self.juvenileFemaleCount =  juvenileFemaleCount
 
 
 page = requests.get('http://www.phila.gov/prisons/page.htm')
@@ -15,10 +16,11 @@ tree = html.fromstring(page.content)
 f = Facility(tree.xpath('//tr[14]/td[1]/text()'),
                 tree.xpath('//tr[14]/td[2]/text()'),
                 tree.xpath('//tr[14]/td[3]/text()'),
-                tree.xpath('//tr[14]/td[4]/text()')
+                tree.xpath('//tr[14]/td[4]/text()'),
+                tree.xpath('//tr[14]/td[5]/text()')
                 )
 
 
-print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount)
+print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount)
 
 
