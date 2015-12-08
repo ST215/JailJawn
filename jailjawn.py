8f601b51e8794cf94db4adbaa359711af85fa6ac
Stanley H Griggs II <shg5004@gmail.com>
2015-12-08 10:48:19 -0500

    Added Workers Male and Female

--- jailjawn.py
+++ jailjawn.py
@@ -3,7 +3,7 @@ import requests
 
 class Facility:
     def __init__(self, facilityName, adultMaleCount, adultFemaleCount, juvenileMaleCount, juvenileFemaleCount,
-                 inCountOutCountMale, inCountOutCountFemale):
+                 inCountOutCountMale, inCountOutCountFemale, workersMale, workersFemale):
 
         self.facilityName =  facilityName
         self.adultMaleCount =  adultMaleCount
@@ -12,6 +12,8 @@ class Facility:
         self.juvenileFemaleCount =  juvenileFemaleCount
         self.inCountOutCountMale =  inCountOutCountMale
         self.inCountOutCountFemale =  inCountOutCountFemale
+        self.workersMale =  workersMale
+        self.workersFemale =  workersFemale
 
 
 page = requests.get('http://www.phila.gov/prisons/page.htm')
@@ -23,11 +25,13 @@ f = Facility(tree.xpath('//tr[14]/td[1]/text()'),
                 tree.xpath('//tr[14]/td[4]/text()'),
                 tree.xpath('//tr[14]/td[5]/text()'),
                 tree.xpath('//tr[14]/td[6]/text()'),
-                tree.xpath('//tr[14]/td[7]/text()')
+                tree.xpath('//tr[14]/td[7]/text()'),
+                tree.xpath('//tr[14]/td[8]/text()'),
+                tree.xpath('//tr[14]/td[9]/text()'),
                 )
 
 
 print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount,
-      f.inCountOutCountMale, f.inCountOutCountFemale)
+      f.inCountOutCountMale, f.inCountOutCountFemale, f.workersMale, f.workersFemale)
 
 
