39bcce39c6073f9026abd5e4524defd5f9471c45
Stanley H Griggs II <shg5004@gmail.com>
2015-12-17 12:33:36 -0500

    Refactored getxpath
    All items put into a generated array then built the facility object
    using the array with a “splat” operator to unpack the array on my
    objects. Mike Helped!

--- jailjawn.py
+++ jailjawn.py
@@ -3,28 +3,18 @@ from lxml import html
 
 from Facility import Facility
 
+unicode_whitespace= u'\xa0'
 page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
 
-
 def getxpath(columnNumber):
-        return tree.xpath('//tr[14]/td[%i]/text()' % columnNumber)
+    return tree.xpath('//tr[16]/td[%i]/text()' % columnNumber)
 
-f = Facility(getxpath(1),
-             getxpath(2),
-             getxpath(3),
-             getxpath(4),
-             getxpath(5),
-             getxpath(6),
-             getxpath(7),
-             getxpath(8),
-             getxpath(9),
-             getxpath(10),
-             getxpath(11),
-             getxpath(12),
-             getxpath(13),
-             getxpath(14),
-             getxpath(15))
+argumentsArray = []
+for i in range(1, 16, 1):
+    argumentsArray.append(getxpath(i))
+
+f = Facility(* argumentsArray)
 
 print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount,
       f.inCountOutCountMale, f.inCountOutCountFemale, f.workersMale, f.workersFemale, f.furloughMale, f.furloughFemale,
