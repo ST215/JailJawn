fcb91ed3acbc9b830c875e17193db89deb641c0e
Stanley H Griggs II <shg5004@gmail.com>
2015-12-21 15:27:08 -0500

    Working Debug commit

--- jailjawn.py
+++ jailjawn.py
@@ -3,16 +3,30 @@ from lxml import html
 
 from Facility import Facility
 
-unicode_whitespace= u'\xa0'
+unicode_whitespace = ''+(u'\xa0')
+
+print type(unicode_whitespace)
+
 page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
 
 def getxpath(columnNumber):
-    return tree.xpath('//tr[16]/td[%i]/text()' % columnNumber)
+    return tree.xpath('//tr[14]/td[%i]/text()' % columnNumber)
 
 argumentsArray = []
 for i in range(1, 16, 1):
     argumentsArray.append(getxpath(i))
+    #print getxpath(i)
+    temp=getxpath(i)
+    print temp
+
+    if temp == unicode_whitespace:
+        print True
+    else:
+        #print type(temp)
+        #print type(unicode_whitespace)
+        #print(getxpath(i))
+        print False
 
 f = Facility(* argumentsArray)
 
