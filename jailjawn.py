766a21654aeec868e87563a11cfd6c35ed335b7f
Maurice Jenkins <mauricejenkins9@gmail.com>
2015-12-22 15:28:15 -0500

    working commit

--- jailjawn.py
+++ jailjawn.py
@@ -8,24 +8,31 @@ page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
 
 def getxpath(columnNumber):
-    return tree.xpath('//tr[14]/td[%i]/text()' % columnNumber)
+    return tree.xpath('//tr[7]/td[%i]/text()' % columnNumber)
+
+def getxypath( columnNumber):
+    return tree.xpath('//tr[j]/td[%i]/text()' % columnNumber)
 
 argumentsArray = []
-for i in range(1, 16, 1):
-    argumentsArray.append(getxpath(i))
+for j in range (7, 30, 1):
+    for i in range(1, 16, 1):
 
-    temp=str(getxpath(i))
-    if temp==unicode_whitespace:
-        print True
-    else:
-        print False
-    
 
-f = Facility(* argumentsArray)
+        temp=str(getxypath(i))
+        print temp
+        if temp==unicode_whitespace:
+            argumentsArray.append(0)
+            #print True
+        else:
+            argumentsArray.append(getxypath(i))
+            #print False
 
-print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount,
+    f = Facility(* argumentsArray)
+    print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount,
       f.inCountOutCountMale, f.inCountOutCountFemale, f.workersMale, f.workersFemale, f.furloughMale, f.furloughFemale,
       f.openWardMale, f.openWardFemale, f.emergTripsMale, f.emergTripsFemale)
+    argumentsArray = []
+
 
 
 #Return string from the list created by getxpath.
