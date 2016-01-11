42ddcad7615335effb7beaea2faf974dc679b740
Stanley H Griggs II <shg5004@gmail.com>
2016-01-11 15:54:01 -0500

    Cleaned Up file and refactored

--- jailjawn.py
+++ jailjawn.py
@@ -2,7 +2,7 @@ import requests
 from lxml import html
 from Facility import Facility
 
-non_interesting_data = "[u" + "'\\" + "xa0']"
+non_interesting_data = ["[u" + "'\\" + "xa0']", "['#REF!']", "[]"]
 page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
 
@@ -10,15 +10,13 @@ tree = html.fromstring(page.content)
 def getxypath(columnNumber, rowNumber):
     return tree.xpath('//tr[%i]/td[%i]/text()' % (columnNumber, rowNumber))
 
-
 argumentsArray = []
-for individual_row_item in range(1, 30, 1):
-    if str(getxypath(individual_row_item, 1)) == '[]' or str(getxypath(individual_row_item, 1)) == non_interesting_data:
-        print "DEBUGGING ....... loop removed the " + str(getxypath(individual_row_item, 1))
+for individual_row_item in range(1, 40, 1):
+    if str(getxypath(individual_row_item, 1)) in non_interesting_data:
         continue
     for individual_column_item in range(1, 16, 1):
         facilityCellData = str(getxypath(individual_row_item, individual_column_item))
-        if facilityCellData == non_interesting_data:
+        if facilityCellData in non_interesting_data:
             argumentsArray.append(['0'])
         else:
             argumentsArray.append(getxypath(individual_row_item, individual_column_item))
@@ -29,9 +27,3 @@ for individual_row_item in range(1, 30, 1):
           f.furloughFemale,
           f.openWardMale, f.openWardFemale, f.emergTripsMale, f.emergTripsFemale)
     argumentsArray = []
-
-
-
-# Dynamic Row checkers
-# Go through Every signle row, start adding things to list of Non interesting shit i.e. Unicode white space, totals, etc.
-# Update the checker (loop) if we encounter non interesting shit do not perform the inner loop, move on to the next one.
