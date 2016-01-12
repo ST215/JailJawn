3aa8c957afc32b90e40b4736f5d03773634140cc
Stanley H Griggs II <shg5004@gmail.com>
2016-01-12 14:49:21 -0500

    Refactored with Mike. M solved Array/String issue stemming getxypath function. Cleaned up.

--- jailjawn.py
+++ jailjawn.py
@@ -2,28 +2,34 @@ import requests
 from lxml import html
 from Facility import Facility
 
-non_interesting_data = ["[u" + "'\\" + "xa0']", "['#REF!']", "[]"]
+non_interesting_data = ['#REF!', '[]', u'\xa0', '0']
 page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
-
+default_value = '0'
 
 def getxypath(columnNumber, rowNumber):
-    return tree.xpath('//tr[%i]/td[%i]/text()' % (columnNumber, rowNumber))
+    value = tree.xpath('//tr[%i]/td[%i]/text()' % (columnNumber, rowNumber))
+    if len(value) == 0:
+        return default_value
+    else:
+        return value[0]
 
 argumentsArray = []
 for individual_row_item in range(1, 40, 1):
-    if str(getxypath(individual_row_item, 1)) in non_interesting_data:
-        continue
-    for individual_column_item in range(1, 16, 1):
-        facilityCellData = str(getxypath(individual_row_item, individual_column_item))
-        if facilityCellData in non_interesting_data:
-            argumentsArray.append(['0'])
-        else:
-            argumentsArray.append(getxypath(individual_row_item, individual_column_item))
+    if not getxypath(individual_row_item, 1) in non_interesting_data:
+        for individual_column_item in range(1, 16, 1):
+            facilityCellData = getxypath(individual_row_item, individual_column_item)
+            if facilityCellData in non_interesting_data:
+                argumentsArray.append(default_value)
+            else:
+                if facilityCellData.isdigit():
+                    argumentsArray.append(float(facilityCellData))
+                else:
+                    argumentsArray.append(facilityCellData.replace('\r\n', ''))
 
-    f = Facility(*argumentsArray)
-    print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount,
-          f.inCountOutCountMale, f.inCountOutCountFemale, f.workersMale, f.workersFemale, f.furloughMale,
-          f.furloughFemale,
-          f.openWardMale, f.openWardFemale, f.emergTripsMale, f.emergTripsFemale)
-    argumentsArray = []
+        f = Facility(*argumentsArray)
+        print(f.facilityName, f.adultMaleCount, f.adultFemaleCount, f.juvenileMaleCount, f.juvenileFemaleCount,
+              f.inCountOutCountMale, f.inCountOutCountFemale, f.workersMale, f.workersFemale, f.furloughMale,
+              f.furloughFemale,
+              f.openWardMale, f.openWardFemale, f.emergTripsMale, f.emergTripsFemale)
+        argumentsArray = []
