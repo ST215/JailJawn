e34e5de823f9fea21028de76df5819bb7259d749
Stanley H Griggs II <shg5004@gmail.com>
2016-02-29 11:42:26 -0500

    Working Commit. Key Value Pairs

--- jailjawn.py
+++ jailjawn.py
@@ -6,6 +6,7 @@ non_interesting_data = ['#REF!', '[]', u'\xa0', '0']
 page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
 default_value = '0'
+keys = ['', '''Facility Name''', '''Adult Male''', '''Adult Female''', '''Juvenile Male''', '''Juvenile Female''', '''In/Out Count Male''','''In/Out Count Female''', '''Worker Male''', '''Worker Female''', '''Furlough Male''', '''Furlough Female''', '''Open Ward Male''', '''Open Ward Female''', '''Emergecy Room Trip Male''', '''Emergecy Room Trip Female''', '''Total''']
 
 def getxypath(columnNumber, rowNumber):
     value = tree.xpath('//tr[%i]/td[%i]/text()' % (columnNumber, rowNumber))
@@ -17,16 +18,19 @@ def getxypath(columnNumber, rowNumber):
         else:
             return value[0]
 
-argumentsArray = []
-for individual_row_item in range(1, 40, 1):
+
+argumentsArray = {}
+for individual_row_item in range(6, 40, 1):
     if not getxypath(individual_row_item, 1) in non_interesting_data:
         for individual_column_item in range(1, 16, 1):
             facilityCellData = getxypath(individual_row_item, individual_column_item)
             if facilityCellData.isdigit():
-                argumentsArray.append(int(facilityCellData))
+                facilityCellData = int(facilityCellData) #argumentsArray.append(int(facilityCellData))
             else:
-                argumentsArray.append(facilityCellData.replace('\r\n', ''))
+                facilityCellData = facilityCellData.replace('\r\n', '') #argumentsArray.append(facilityCellData.replace('\r\n', ''))
+
+            argumentsArray[keys[individual_column_item]] = facilityCellData
+        print(argumentsArray)
 
         f = Facility(*argumentsArray)
-        f.print_description()
-        argumentsArray = []
+        #f.print_description()
