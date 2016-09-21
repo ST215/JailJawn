418be5431f82d43d1c3b79443c4a586036734c72
Stanley H Griggs II <shg5004@gmail.com>
2016-09-21 18:35:02 -0400

    Worked with Diego in RCStart from the Recurse Center.

--- jailjawn.py
+++ jailjawn.py
@@ -9,15 +9,28 @@ page = requests.get('http://www.phila.gov/prisons/page.htm')
 tree = html.fromstring(page.content)
 default_value = '0'
 keys = ['', 'Facility Name', 'Adult Male', 'Adult Female', 'Juvenile Male', 'Juvenile Female', 'In Out Male','In Out Female', 'Worker Male', 'Worker Female', 'Furlough Male', 'Furlough Female', 'Open Ward Male', 'Open Ward Female', 'Emergecy Room Trip Male', 'Emergecy Room Trip Female', 'Total Count']
-FIREBASE_URL = "https://burning-heat-7610.firebaseio.com/"
+FIREBASE_URL = "https://jailjawndev.firebaseio.com/"
 fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
 
 
 # Main
 if __name__ == '__main__':
+
+
+    def readValue(element):
+        value = element.text
+        if not value: return default_value
+        if len(value) == 0:
+            return default_value
+        else:
+            if value[0] in non_interesting_data:
+                return default_value
+            else:
+                return value[0]
+
     def getxypath(columnNumber, rowNumber):
         value = tree.xpath('//tr[%i]/td[%i]/text()' % (columnNumber, rowNumber))
-        #print value
+        # print value
         if len(value) == 0:
             return default_value
         else:
@@ -27,35 +40,30 @@ if __name__ == '__main__':
                 return value[0]
 
 
+    def getRows():
+        rows = tree.xpath('//tr')
+        return rows[6:40]
+
+
     dateOnPrisonCensus = datetime.strptime(getxypath(2, 2), "%B %d, %Y").date()
 
-    argumentsArray = {}
-    for individual_row_item in range(6, 40, 1):
-        if not getxypath(individual_row_item, 1) in non_interesting_data:
-            for individual_column_item in range(1, 17, 1):
-                facilityCellData = getxypath(individual_row_item, individual_column_item)
-                if facilityCellData.isdigit():
-                    facilityCellData = int(facilityCellData)
-                else:
-                    facilityCellData = facilityCellData.replace('\r\n', ' ').replace('.', ' ').replace(' ', ' ').replace('\n', ' ')
+    print dateOnPrisonCensus
 
-                argumentsArray[keys[individual_column_item]] = facilityCellData
+    def prisonParser(acc, row):
+        facilityname = row[0].text
+        columnElements = row[1:16]
+        columnTexts = [row[0].text] + map(readValue, columnElements)
 
-            data = {'Facility Name' : argumentsArray['Facility Name'],
-                        'Adult Male' : argumentsArray['Adult Male'],
-                        'Adult Female' : argumentsArray['Adult Female'],
-                        'Juvenile Male' : argumentsArray['Juvenile Male'],
-                        'Juvenile Female' : argumentsArray['Juvenile Female'],
-                        'In Out Male' : argumentsArray['In Out Male'],
-                        'In Out Female' : argumentsArray['In Out Female'],
-                        'Worker Male' : argumentsArray['Worker Male'],
-                        'Worker Female' : argumentsArray['Worker Female'],
-                        'Furlough Male' : argumentsArray['Furlough Male'],
-                        'Furlough Female' : argumentsArray['Furlough Female'],
-                        'Open Ward Male' : argumentsArray['Open Ward Male'],
-                        'Open Ward Female' : argumentsArray['Open Ward Female'],
-                        'Emergecy Room Trip Male' : argumentsArray['Emergecy Room Trip Male'],
-                        'Emergecy Room Trip Female' : argumentsArray['Emergecy Room Trip Female'],
-                        'Total Count' : argumentsArray['Total Count']}
+        acc[facilityname] = { key: value for key,value in zip(keys[1:], columnTexts) }
+        return acc
 
-            fb.put(str(dateOnPrisonCensus), argumentsArray['Facility Name'], data)
+
+    def parseTable():
+        rows = getRows()
+        prisons = reduce(prisonParser, rows, {})
+        print prisons
+        fb.post(str(dateOnPrisonCensus), 'ASD Cannery', prisons)
+        # iterate over all the rows and call prisonParser on them
+        # Aggregate all PrisonParsers' results and create a bigger object
+        # Return
+    parseTable()
